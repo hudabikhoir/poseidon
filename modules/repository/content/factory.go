7802848374ff8a8eac03d03d8114212ce8d3ad39
hudabikhoir <hudaparodi@gmail.com>
2022-02-16 11:18:52 +0700

    feat: sample implementation mongodb
    Closes #11

--- modules/repository/content/factory.go
+++ modules/repository/content/factory.go
@@ -8,11 +8,11 @@ import (
 //RepositoryFactory Will return business.content.Repository based on active database connection
 func RepositoryFactory(dbCon *util.DatabaseConnection) content.Repository {
 	var contentRepo content.Repository
-	// if dbCon.Driver == util.MySQL {
-	contentRepo = NewMySQLRepository(dbCon.PostgreSQL)
-	// } else if dbCon.Driver == util.MongoDB {
-	// 	contentRepo = NewMongoDBRepository(dbCon.MongoDB)
-	// }
+	if dbCon.Driver == util.MySQL {
+		contentRepo = NewMySQLRepository(dbCon.PostgreSQL)
+	} else if dbCon.Driver == util.MongoDB {
+		contentRepo = NewMongoDBRepository(dbCon.MongoDB)
+	}
 
 	return contentRepo
 }
